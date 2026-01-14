d6d2fbf6028d338ca43b0b505031bf794a62411c
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-14 12:01:02 +0800

    Update mission-vision-widget.jsx

--- components/components-mission-vision/mission-vision-widget.jsx
+++ components/components-mission-vision/mission-vision-widget.jsx
@@ -1,6 +1,6 @@
 export default function MissionVisionWidget(title,description) {
     return(
-        <div>
+        <div className="flex flex-col gap-2 justify-center items-center bg-amber-300">
             <h1>{title}</h1>
             {/* <p>{description}</p> */}
         </div>
