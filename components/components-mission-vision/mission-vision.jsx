8e53b15f100b470ae23ecd8ee1814208323f3fad
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-14 22:31:17 +0800

    did the header and finished the widget design

--- components/components-mission-vision/mission-vision.jsx
+++ components/components-mission-vision/mission-vision.jsx
@@ -1,10 +1,23 @@
 import CorporateGovernanceHeader from "../corporate-governance-header";
+import MissionVisionWidget from "./mission-vision-widget";
+import { missionVisionData } from "@/lib/data";
 
 export default function MissionVision() {
   return (
-    <section className="w-full mx-auto max-w-6xl">
-        <CorporateGovernanceHeader/>
-        <MissionVision/>
+    <section className="flex gap-10 
+    justify-center items-center
+    w-full mx-auto max-w-6xl">
+
+      <MissionVisionWidget title={missionVisionData[0].title}
+        description={missionVisionData[0].description}
+        customClassName={"bg-zinc-300"} type="mission"
+      />
+      <MissionVisionWidget title={missionVisionData[1].title}
+        description={missionVisionData[1].description}
+        customClassName={"bg-amber-300"} type="vison"
+      />
+
+
     </section>
   )
 }
