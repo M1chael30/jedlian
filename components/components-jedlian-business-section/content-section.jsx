59e726956343b613063ac644052aba6cb7bad047
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-01-15 14:24:40 +0800

    added icons in data.js

--- components/components-jedlian-business-section/content-section.jsx
+++ components/components-jedlian-business-section/content-section.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function ContentSection({ item }) {
  return (
   <div className="p-6 md:p-18 lg:px-48 lg:py-20 space-y-12 flex flex-col">
@@ -13,19 +15,30 @@ export default function ContentSection({ item }) {
    {/* mid section */}
    <div className="space-y-2">
     {/* title */}
-    <h1 className="font-bold text-xl md:text-2xl lg:text-3xl">{item.titleTwo}</h1>
+    <h1 className="font-bold text-xl md:text-2xl lg:text-3xl">
+     {item.titleTwo}
+    </h1>
     {/* description */}
     <p className="text-xs md:text-sm">{item.descriptionThree}</p>
    </div>
    {/* bottom section */}
    <div className="space-y-2">
     {/* title */}
-    <h1 className="font-bold text-xl md:text-2xl lg:text-3xl">{item.titleThree}</h1>
+    <h1 className="font-bold text-xl md:text-2xl lg:text-3xl">
+     {item.titleThree}
+    </h1>
     {/* description */}
     <p className="text-xs md:text-sm">{item.descriptionFour}</p>
    </div>
    {/* icon */}
-   <div className=""></div>
+   <div className="flex space-x-4">
+    {item.icon &&
+     item.icon.map((Icon, index) => (
+      <Link href={"#"} key={index}>
+       <Icon size={32} />
+      </Link>
+     ))}
+   </div>
   </div>
  );
 }
