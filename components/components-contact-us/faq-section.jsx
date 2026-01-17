882481f3222e3e5186384dd2f165f60943593b5f
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-17 11:56:28 +0800

    Create faq-section.jsx

---
+++ components/components-contact-us/faq-section.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FaqSection() {
+  return (
+                <div className="md:col-span-2 font-extrabold text-4xl bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)]
+             text-transparent bg-clip-text border-b-2 border-b-[#c89116] py-5">
+                <h1>FAQ</h1>
+            </div>
+  )
+}
