6d4517a417c1a4e5317a687717c0bfbcc09e9bdf
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-17 13:45:19 +0800

    finished the faq section

--- components/components-contact-us/contact-us-section.jsx
+++ components/components-contact-us/contact-us-section.jsx
@@ -4,10 +4,12 @@ import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { BsTelephone } from "react-icons/bs";
 import { MdOutlineEmail } from "react-icons/md";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
 
 export default function ContactUsSection() {
   return (
-          <div className="grid md:grid-cols-2 p-5 space-x-8 space-y-6">
+          <div className="grid md:grid-cols-2 space-x-8 space-y-6 w-full">
             <div className="md:col-span-2 font-extrabold text-4xl bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)]
              text-transparent bg-clip-text border-b-2 border-b-[#c89116] py-5">
                 <h1>Contact Us</h1>
@@ -45,7 +47,7 @@ export default function ContactUsSection() {
         </div>
         <div className="space-y-3 md:space-y-0">
           <div className=" flex flex-col md:py-5">
-            <div className="w-full md:h-70 h-50">
+            <div className="w-145 md:h-70 h-50">
               <iframe
                 src={contactsData.mapsLink}
                 allowFullScreen=""
@@ -55,7 +57,9 @@ export default function ContactUsSection() {
               ></iframe>
             </div>
           </div>
-          <div className="bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] text-transparent bg-clip-text">
+          <div className="bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)]
+           text-transparent bg-clip-text flex items-center gap-2 justify-center">
+            <FaMapMarkerAlt className="text-[#c89116]"/>
             {contactsData.address}
           </div>
         </div>
--- components/components-contact-us/faq-section.jsx
+++ components/components-contact-us/faq-section.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
+import { faqData } from "@/lib/contactUsData";
 
 export default function FaqSection() {
   return (
-                <div className="md:col-span-2 font-extrabold text-4xl bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)]
-             text-transparent bg-clip-text border-b-2 border-b-[#c89116] py-5">
-                <h1>FAQ</h1>
-            </div>
-  )
+    <section className="w-full">
+      <div>
+        <div
+          className="md:col-span-2 font-extrabold text-4xl bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)]
+             text-transparent bg-clip-text border-b-2 border-b-[#c89116] py-5 mb-3"
+        >
+          <h1>FAQ</h1>
+                  <p className="text-lg
+                  bg-[radial-gradient(circle_at_center,#E2D7D7,#958D8D,#E9DFDF)]
+             text-transparent bg-clip-text
+                  ">Do you have any questions for us?</p>
+        </div>
+      </div>
+      <div className="space-y-3">
+        {faqData.map((item, index) => (
+          <div key={index} className="flex flex-col">
+            <a href="" className="bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)]
+             text-transparent bg-clip-text border-b-2">{item.question}</a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
