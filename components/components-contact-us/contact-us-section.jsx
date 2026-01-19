cd301c39e14efef6c90f44eeb43237bd04ba7ba9
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-01-19 15:01:41 +0800

    edited contact-us-section.jsx and contact-us page

--- components/components-contact-us/contact-us-section.jsx
+++ components/components-contact-us/contact-us-section.jsx
@@ -2,10 +2,10 @@ import ContactUsAddress from "./contact-us-address";
 import ContactUsDetails from "./contact-us-details";
 
 export default function ContactUsSection() {
-  return (
-    <div className="flex flex-col md:flex-row gap-6 mx-auto w-full max-w-6xl">
-      <ContactUsDetails />
-      <ContactUsAddress />
-    </div>
-  );
+ return (
+  <div className="flex flex-col md:flex-row gap-6 mx-auto w-full max-w-6xl">
+   <ContactUsDetails />
+   <ContactUsAddress />
+  </div>
+ );
 }
