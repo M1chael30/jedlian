27bbd1377aabd7eef6e3e990d34f087744fe360c
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-20 16:13:23 +0800

    adjusted the responsiveness

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -7,8 +7,8 @@ export default function AboutUs() {
   return (
     <main className="max-w-7xl w-full mx-auto">
       <TopSection />
-      {/* <LeadersSection/> */}
-      <div className="w-full bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] h-3"/>
+      <LeadersSection/>
+      <div className="w-full bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] h-3 my-5"/>
       <GeoFootPrintSection/>
       <QuoteSection/>
     </main>
