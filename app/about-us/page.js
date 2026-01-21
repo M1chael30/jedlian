b96214e827656f997a88e42c4c24d4642ced4938
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-01-21 10:33:54 +0800

    testing design

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -2,14 +2,18 @@ import TopSection from "@/components/components-about-us-page/top-section";
 import LeadersSection from "@/components/components-about-us-page/leaders-section";
 import GeoFootPrintSection from "@/components/components-about-us-page/geo-footprint-section";
 import QuoteSection from "@/components/components-about-us-page/quote-section";
-import ContentSection from "@/components/components-about-us-page/content-section";
+import {
+ ContentSectionMobile,
+ ContentSection,
+} from "@/components/components-about-us-page/content-section";
 
 export default function AboutUs() {
  return (
   <main className="max-w-7xl w-full mx-auto">
    <TopSection />
    {/* <LeadersSection/> */}
-   <ContentSection />
+   {/* <ContentSection /> */}
+   <ContentSectionMobile />
    <div className="w-full bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] h-3" />
    {/* <GeoFootPrintSection/>
       <QuoteSection/> */}
--- components/components-about-us-page/content-section.jsx
+++ components/components-about-us-page/content-section.jsx
@@ -1,28 +1,42 @@
 import React from "react";
 
-export default function ContentSection() {
+export function ContentSection() {
+ return (
+  <div className="p-5 w-full">
+   {/* container */}
+   <div>
+    {/* left section */}
+    <div className="bg-muted/50 aspect-square rounded-lg" />
+    {/* right section */}
+    <div className="flex justify-center items-center flex-col gap-8">
+     <div className="border-t border-r border-b border-amber-500 rounded-r-lg py-4">
+      {/* banner */}
+      <div className="w-full bg-amber-500 px-4 py-2 rounded-r-lg">
+       <p>signature</p>
+       <p>CEO/PRESIDENT</p>
+      </div>
+      {/* content */}
+      <div className="px-6">
+       <p className="text-description">
+        Jedlian Holdings Inc. is a diversified parent company that oversees and
+        strategically manages five thriving business enterprises, each
+        contributing to the organization’s continued growth and reputation for
+        excellence: Jedlian Communication, Jedlian Construction, Jedlian
+        Gasoline, Jedlian Drugstore, and Casa Jedliana Hotel and Resort.
+       </p>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+ );
+}
+
+export function ContentSectionMobile() {
  return (
   <div className="p-5 w-full mx-auto max-w-3xl">
    {/* container */}
-   <div className="relative flex">
-    {/* banner */}
-    <div className="hidden absolute">
-     <p>signature</p>
-     <p>CEO/PRESIDENT</p>
-    </div>
-    {/* left section */}
-    <div className="w-full md:w-1/3 bg-red-500 aspect-square h-90 rounded-lg" />
-    {/* right section */}
-    <div className="w-full md:w-10/12">
-     <p>
-      Jedlian Holdings Inc. is a diversified parent company that oversees and
-      strategically manages five thriving business enterprises, each
-      contributing to the organization’s continued growth and reputation for
-      excellence: Jedlian Communication, Jedlian Construction, Jedlian Gasoline,
-      Jedlian Drugstore, and Casa Jedliana Hotel and Resort.
-     </p>
-    </div>
-   </div>
+  
   </div>
  );
 }
