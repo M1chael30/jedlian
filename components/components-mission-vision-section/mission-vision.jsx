87e25332ddf2c8a2a4b7979b9edf2941b6c78b13
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-21 14:44:16 +0800

    changed design of mission vision

--- components/components-mission-vision-section/mission-vision.jsx
+++ components/components-mission-vision-section/mission-vision.jsx
@@ -1,26 +1,26 @@
 import { missionVisionData } from "@/lib/corporateGovernanceData";
 import MissionVisionWidget from "./mission-vision-widget";
+import { PiTargetFill } from "react-icons/pi";
+import { BsEyeFill } from "react-icons/bs";
 
 export default function MissionVision() {
- return (
-  <section
-   className="
+  return (
+    <section
+      className="
         grid grid-cols-1 md:grid-cols-2
-       gap-8 px-8 my-12"
-  >
-   <MissionVisionWidget
-    title={missionVisionData[0].title}
-    description={missionVisionData[0].description}
-    customClassName="bg-zinc-300/50 w-full max-w-md"
-    type="mission"
-   />
+     gap-5 px-8 mb-12 justify-center items-center text-center"
+    >
 
-   <MissionVisionWidget
-    title={missionVisionData[1].title}
-    description={missionVisionData[1].description}
-    customClassName="bg-amber-300/70 w-full max-w-md"
-    type="vision"
-   />
-  </section>
- );
+        <div className="flex flex-col items-center">
+          <PiTargetFill className="text-9xl text-yellow-400"/>
+          <h1 className="uppercase text-title text-yellow-400">{missionVisionData[0].title}</h1>
+          <p className="text-description">{missionVisionData[0].description}</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <BsEyeFill className="text-9xl text-yellow-400"/>
+          <h1 className="uppercase text-title text-yellow-400">{missionVisionData[1].title}</h1>
+          <p className="text-description">{missionVisionData[1].description}</p>
+        </div>
+    </section>
+  );
 }
