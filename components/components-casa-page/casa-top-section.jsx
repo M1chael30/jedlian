d785be2183b0e6de2de0f9bdc78df50bf56fbcb9
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-01-22 11:50:13 +0800

    created casa-top-section.jsx and casaData.js

--- components/components-casa-page/casa-top-section.jsx
+++ components/components-casa-page/casa-top-section.jsx
@@ -1,7 +1,40 @@
+import { casaInformation } from "@/lib/casaData";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
+
 export default function CasaTopSection() {
  return (
-  <div className="px-5">
-   <div className="relative text-black w-full">Casa top section</div>
+  <div className="flex items-center justify-center overflow-hidden">
+   <div className="grid lg:grid-cols-2 gap-12 px-6 py-10 lg:py-0">
+    <div className="my-auto">
+     <p className="max-w-[60ch] text-lg text-[#45291f] font-semibold">
+      _Jedlian Holdings Inc.
+     </p>
+     <h1 className="mt-6 max-w-[10ch] text-4xl md:text-5xl lg:text-[2.75rem] xl:text-[3.25rem] font-semibold leading-[1.2]! tracking-[-0.035em] text-[#45291f]">
+      MY HOME IS YOUR HOME
+     </h1>
+
+     <div className="space-y-3 mt-14">
+      {casaInformation.map((item, index) => (
+       <p
+        key={index}
+        className="max-w-[45ch] text-sm text-[#45291f] border-b border-[#45291f] flex items-center gap-2"
+       >
+        {item.icon && <item.icon size={18} />}
+        {item.tile}
+       </p>
+      ))}
+     </div>
+
+     <Button className="border-2 border-[#45291f] bg-[#f8f5f0] rounded-none mt-8 hover:bg-[#f8f5f0]">
+      Book here <ArrowRight />
+     </Button>
+    </div>
+    <div className="w-full aspect-video lg:w-250 lg:rounded-none flex">
+     <div className="bg-accent flex-6 rounded-xl lg:rounded-none" />
+     <div className="bg-[#ca9c57] flex-5 hidden lg:block" />
+    </div>
+   </div>
   </div>
  );
 }
