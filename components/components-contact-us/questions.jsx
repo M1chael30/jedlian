b97d037d1df120b5ca7a9346c0714a2c64b16726
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-22 14:11:20 +0800

    started changing animations on contact us

--- components/components-contact-us/questions.jsx
+++ components/components-contact-us/questions.jsx
@@ -9,35 +9,6 @@ import { faqData } from "@/lib/contactUsData";
 import { motion } from "framer-motion";
 
 export default function Questions() {
-<<<<<<< Updated upstream
- return (
-  <motion.div
-   variants={sectionVariants}
-   initial="hidden"
-   whileInView="show"
-   viewport={{ once: true, amount: 0.3 }}
-  >
-   <Accordion type="single" collapsible className="w-full" defaultValue="">
-    {faqData.map((item, index) => (
-     <AccordionItem key={index} value={`item-${index}`}>
-      <AccordionTrigger>
-       <motion.p
-        className="bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)]
-      text-transparent bg-clip-text font-extrabold text-description"
-        variants={itemVariants}
-       >
-        {item.question}
-       </motion.p>
-      </AccordionTrigger>
-      <AccordionContent className="flex flex-col gap-4 text-balance">
-       <motion.p className="text-description">{item.answer}</motion.p>
-      </AccordionContent>
-     </AccordionItem>
-    ))}
-   </Accordion>
-  </motion.div>
- );
-=======
   return (
     <div>
       <Accordion type="single" collapsible className="w-full" defaultValue="">
@@ -59,5 +30,4 @@ export default function Questions() {
       </Accordion>
     </div>
   );
->>>>>>> Stashed changes
 }
