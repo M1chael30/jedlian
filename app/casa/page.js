abe24e171d2eb29913af6431886d28b0cc197fda
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-01-23 11:40:44 +0800

    created the classification section

--- app/casa/page.js
+++ app/casa/page.js
@@ -1,13 +1,12 @@
 import CasaClassificationSection from "@/components/components-casa-page/casa-classification-section";
 import CasaTopSection from "@/components/components-casa-page/casa-top-section";
-import { casaClassificationData } from "@/lib/casaData";
 
 export default function Casa() {
  return (
   <section className="w-full">
    <div className="mx-auto w-full max-w-7xl bg-primary">
     <CasaTopSection />
-    <CasaClassificationSection items={casaClassificationData} />
+    <CasaClassificationSection />
    </div>
   </section>
  );
