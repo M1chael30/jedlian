e72064907cca9d562a0aea757ce05f8d54698475
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-01-23 16:24:10 +0800

    Update content-section.jsx

--- components/components-jedlian-business-section/content-section.jsx
+++ components/components-jedlian-business-section/content-section.jsx
@@ -3,10 +3,10 @@ import CustomButton from "../ui/custom-button";
 import { ArrowRight } from "lucide-react";
 import { motion } from "framer-motion";
 import {
-  itemVariants,
-  fadeVariants,
-  sectionVariants,
-  fadeRightVariants,
+ itemVariants,
+ fadeVariants,
+ sectionVariants,
+ fadeRightVariants,
 } from "@/lib/animations";
 
 export default function ContentSection({ item }) {
@@ -16,106 +16,95 @@ export default function ContentSection({ item }) {
    initial="hidden"
    whileInView="show"
    viewport={{ once: true, amount: 0.3 }}
-   className="p-6 md:p-18 lg:px-48 lg:py-20 space-y-12 flex flex-col"
+   className="p-4 md:p-18 lg:px-48 lg:py-5 space-y-12 flex flex-col"
   >
    {/* top section */}
    <div>
     {/* title */}
     <motion.h1
-     variants={itemVariants}
-     className="pb-2 font-bold bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] bg-clip-text text-transparent text-title max-w-[15ch]"
+     variants={fadeRightVariants}
+     initial="hidden"
+     animate="show"
+     className="py-15 font-bold bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] bg-clip-text text-transparent text-title"
     >
-      {/* top section */}
-      <div>
-        {/* title */}
-        <motion.h1
-          variants={fadeRightVariants}
-          initial="hidden"
-          animate="show"
-          className="py-15 font-bold bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] bg-clip-text text-transparent text-title"
-        >
-          {item?.title}
-        </motion.h1>
-        {/* description */}
-        <motion.p
-          variants={fadeVariants}
-          initial="hidden"
-          whileInView="show"
-          className="text-description text-justify mb-5"
-        >
-          {item?.descriptionTwo}
-        </motion.p>
-        {/* button  */}
-        {item?.title === "Casa Jedliana" && (
-          <motion.div
-            variants={fadeVariants}
-            whileInView="show"
-            initial="hidden"
-          >
-            <CustomButton
-              icon={<ArrowRight size={18} />}
-              buttonText="Learn more about casa"
-              buttonHref="/casa"
-            />
-          </motion.div>
-        )}
-      </div>
-      {/* mid section */}
-      <div className="space-y-2">
-        {/* title */}
-        <motion.h1
-          variants={fadeVariants}
-          initial="hidden"
-          whileInView="show"
-          className="font-bold text-subtitle"
-        >
-          {item?.titleTwo}
-        </motion.h1>
-        {/* description */}
-        <motion.p
-          variants={fadeVariants}
-          initial="hidden"
-          whileInView="show"
-          className="text-description text-justify"
-        >
-          {item?.descriptionThree}
-        </motion.p>
-      </div>
-      {/* bottom section */}
-      <div className="space-y-2">
-        {/* title */}
-        <motion.h1
-          variants={fadeVariants}
-          initial="hidden"
-          whileInView="show"
-          className="font-bold text-subtitle"
-        >
-          {item?.titleThree}
-        </motion.h1>
-        {/* description */}
-        <motion.p
-          variants={fadeVariants}
-          initial="hidden"
-          whileInView="show"
-          className="text-description text-justify"
-        >
-          {item?.descriptionFour}
-        </motion.p>
-      </div>
-      {/* icon */}
-      <motion.div
-        variants={fadeVariants}
-        initial="hidden"
-        whileInView="show"
-        className="flex space-x-4"
-      >
-        {item?.socials &&
-          item.socials.map((item, index) => (
-            <Link href={item.socialLink} key={index}>
-              <item.icon size={32} />
-            </Link>
-          ))}
-      </motion.div>
-    </motion.div>
-  );
+     {item?.title}
+    </motion.h1>
+    {/* description */}
+    <motion.p
+     variants={fadeVariants}
+     initial="hidden"
+     whileInView="show"
+     className="text-description text-justify mb-5"
+    >
+     {item?.descriptionTwo}
+    </motion.p>
+    {/* button  */}
+    {item?.title === "Casa Jedliana" && (
+     <motion.div variants={fadeVariants} whileInView="show" initial="hidden">
+      <CustomButton
+       icon={<ArrowRight size={18} />}
+       buttonText="Learn more about casa"
+       buttonHref="/casa"
+      />
+     </motion.div>
+    )}
+   </div>
+   {/* mid section */}
+   <div className="space-y-2">
+    {/* title */}
+    <motion.h1
+     variants={fadeVariants}
+     initial="hidden"
+     whileInView="show"
+     className="font-bold text-subtitle"
+    >
+     {item?.titleTwo}
+    </motion.h1>
+    {/* description */}
+    <motion.p
+     variants={fadeVariants}
+     initial="hidden"
+     whileInView="show"
+     className="text-description text-justify"
+    >
+     {item?.descriptionThree}
+    </motion.p>
+   </div>
+   {/* bottom section */}
+   <div className="space-y-2">
+    {/* title */}
+    <motion.h1
+     variants={fadeVariants}
+     initial="hidden"
+     whileInView="show"
+     className="font-bold text-subtitle"
+    >
+     {item?.titleThree}
+    </motion.h1>
+    {/* description */}
+    <motion.p
+     variants={fadeVariants}
+     initial="hidden"
+     whileInView="show"
+     className="text-description text-justify"
+    >
+     {item?.descriptionFour}
+    </motion.p>
+   </div>
+   {/* icon */}
+   <motion.div
+    variants={fadeVariants}
+    initial="hidden"
+    whileInView="show"
+    className="flex space-x-4"
+   >
+    {item?.socials &&
+     item.socials.map((item, index) => (
+      <Link href={item.socialLink} key={index}>
+       <item.icon size={32} />
+      </Link>
+     ))}
+   </motion.div>
+  </motion.div>
+ );
 }
