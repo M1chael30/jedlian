a3792648a1d6be3e34ddc03b6b1331a9f010a95a
JhibJhib <jhibrielbf2004@gmail.com>
2026-01-23 13:41:20 +0800

    Update mission-vision-widget.jsx

--- components/components-mission-vision-section/mission-vision-widget.jsx
+++ components/components-mission-vision-section/mission-vision-widget.jsx
@@ -9,15 +9,31 @@ export default function MissionVisionWidget({
 }) {
   return (
     <motion.div
-      // initial={{ opacity: 0, y: 20 }}
-      // animate={{ opacity: 1, transition: {duration:1}, y:0 }}
       variants={missionVisionVariants}
       className="border border-yellow-400 rounded-4xl p-10"
     >
+      <svg width="0" height="0">
+        <defs>
+          <linearGradient id="gold-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stopColor="#c89116" />
+            <stop offset="50%" stopColor="#c89116" />
+            <stop offset="100%" stopColor="#cfceaa" />
+          </linearGradient>
+        </defs>
+      </svg>
+
       <div className="flex flex-col items-center">
-        <Icon className="text-7xl md:text-9xl text-yellow-400" />
-        <h1 className="uppercase text-title text-yellow-400">{title}</h1>
+        <Icon
+          className="text-7xl md:text-9xl"
+          style={{ fill: "url(#gold-gradient)" }}
+        />
+
+        <h1 className="uppercase text-title bg-[radial-gradient(circle_at_75%,#c89116,#c89116,#cfceaa)]
+             text-transparent bg-clip-text ">
+          {title}
+        </h1>
       </div>
+
       <p className="text-description">{description}</p>
     </motion.div>
   );
